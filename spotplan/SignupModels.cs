ce2e2c4f28b3608a387c57e92fb6d329cb126a01
Evad <torres.kim.dave@gmail.com>
2021-06-20 14:25:17 +0800

    Added signupmodels.

---
+++ spotplan/SignupModels.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace spotplan
+{
+    class SignupModels
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

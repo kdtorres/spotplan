d89797d2d61b050306e81685a0f41b6ce4de7960
Evad <torres.kim.dave@gmail.com>
2021-06-27 03:01:11 +0800

    Crypto list add function

--- spotplan/MainContentActivity.cs
+++ spotplan/MainContentActivity.cs
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace spotplan
 {
-    [Activity(Label = "MainContentActivity")]
+    [Activity(Label = "")]
     public class MainContentActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -23,10 +23,28 @@ namespace spotplan
             // Set our view from layout resource
             SetContentView(Resource.Layout.activity_content_main);
 
-              
-            
-            Button btn = FindViewById<Button>(Resource.Id.btn1);
-            btn.Click += async (Sender, e) =>
+            // favorate crypto list btn
+            Button btn1 = FindViewById<Button>(Resource.Id.btn1);
+            btn1.Click += async (Sender, e) =>
+            {
+             
+                var intent = new Intent(this, typeof(FavorateCryptoActivity));
+                await Task.Delay(1000);
+                StartActivity(intent);
+            };
+
+            // income planner btn
+            Button btn2 = FindViewById<Button>(Resource.Id.btn2);
+            btn2.Click += async (Sender, e) =>
+            {
+                await Task.Delay(1000);
+                var intent = new Intent(this, typeof(SignupActivity));
+                StartActivity(intent);
+            };
+
+            // crypto prices btn
+            Button btn3 = FindViewById<Button>(Resource.Id.btn3);
+            btn3.Click += async (Sender, e) =>
             {
                 await Task.Delay(1000);
                 var intent = new Intent(this, typeof(SignupActivity));
